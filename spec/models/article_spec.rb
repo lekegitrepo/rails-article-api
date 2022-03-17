713f31dc899c8f40a9d183ff205ab0c7becb8a9c
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-03-17 08:39:05 +0100

    Add validation test for article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "create an instance of an article" do
-    article = create(:article)
-    expect(article).to be_an(Object)
-    expect(article.title).to be_a(String)  
-  end
+  describe '#validation' do
+    let(:article) { build(:article) }
+ 
+    it "test article validity" do
+      expect(article).to be_valid  
+    end
+
+    it "check article validity" do
+      article.title = ""
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
     
+  end
 end
