b7f2f474a1b0ffffc3b909378fff4e3a08704688
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-03-21 08:25:57 +0100

    Add validation for the content and slug

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,10 +8,22 @@ RSpec.describe Article, type: :model do
       expect(article).to be_valid  
     end
 
-    it "check article validity" do
+    it "check article title validity" do
       article.title = ""
       expect(article).not_to be_valid
       expect(article.errors[:title]).to include("can't be blank")
+    end
+    
+    it "check article content validity" do
+      article.content = ""
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it "check article slug validity" do
+      article.slug = ""
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
     end
     
   end
