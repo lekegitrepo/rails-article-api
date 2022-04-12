7aabbc88bfc1b5365c0250524f425c77ee59219d
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-04-12 09:55:05 +0100

    Add pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,9 @@
 class ArticlesController < ApplicationController
+  include Pagination
   def index
     articles = Article.recent
-    paginated = paginator.call(
-      articles,
-      params: pagination_params,
-      base_url: request.url
-    )
-    
-    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
-    
-    render json: ArticleSerializer.new(paginated.items, options), status: :ok
+    paginated = paginable(articles)
+    render_collection(paginated)
   end
 
   def show
@@ -20,14 +14,5 @@ class ArticlesController < ApplicationController
 
   def serializer
     ArticleSerializer
-  end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-  
-
-  def pagination_params
-    params.permit![:page]
   end
 end
