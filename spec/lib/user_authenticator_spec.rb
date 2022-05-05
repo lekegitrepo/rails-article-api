9ffbc35a86ab3264ada770d7c2cf008160b48b9f
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-05-05 08:47:54 +0100

    Update user authentication test

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UserAuthenticator do
   describe "#perform" do
+    let(:authenticator) { described_class.new('sample_code') } 
+    subject { authenticator.perform } 
     context "when the code is incorrect" do
       it "should raise an error" do
-        authenticator = described_class.new('sample_code')
-        expect { authenticator.perform }.to raise_error(
+        expect { subject }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
       end
     end
+
+    context "when the code is valid/correct" do
+      it "should save new user" do
+        expect{ subject }.to change { User.count }.by(1)
+      end
+      
+    end
+    
   end
 end
