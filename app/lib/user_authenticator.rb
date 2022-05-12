affac4fc3cf196e631119eb2ad15e6c1aefd39e4
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-05-12 08:24:01 +0100

    Update authenticator specs

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -12,12 +12,14 @@ class UserAuthenticator
       client_id: ENV['GITHUB_CLIENT_ID'],
       client_secret: ENV['GITHUB_CLIENT_SECRET']
     )
-    res = client.exchange_code_for_token(code)
-    if res[:errors].present?
+    token = client.exchange_code_for_token(code)
+    if token.try(:error).present?
       raise AuthenticationError 
     else   
     end
   end
+
+  private
 
   attr_reader :code
   
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UserAuthenticator do
   describe "#perform" do
-    let(:authenticator) { described_class.new('sample_code') } 
+    let(:authenticator) { described_class.new('sample_code') }
+
     subject { authenticator.perform } 
+
     context "when the code is incorrect" do
+      let(:error) { double("Sawyer::Resource", error: "bad_verification_code") }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
+
       it "should raise an error" do
         expect { subject }.to raise_error(
           UserAuthenticator::AuthenticationError
@@ -14,6 +23,11 @@ RSpec.describe UserAuthenticator do
     end
 
     context "when the code is valid/correct" do
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
+
       it "should save new user" do
         expect{ subject }.to change { User.count }.by(1)
       end
